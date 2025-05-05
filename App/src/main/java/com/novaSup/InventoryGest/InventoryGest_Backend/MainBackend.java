f806714961fcde2e699b5a146feeb6acb091f5f4
Neid-09 <neiderqm09@gmail.com>
2025-05-04 19:38:40 -0500

    Limpiar el main de backend

--- App/src/main/java/com/novaSup/InventoryGest/InventoryGest_Backend/MainBackend.java
+++ App/src/main/java/com/novaSup/InventoryGest/InventoryGest_Backend/MainBackend.java
@@ -3,18 +3,13 @@ package com.novaSup.InventoryGest.InventoryGest_Backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-// Asegúrate de que WebApplicationType esté configurado como SERVLET o REACTIVE
-// import org.springframework.boot.WebApplicationType;
 
 @SpringBootApplication
-@EnableScheduling // Mantener si el backend necesita tareas programadas
+@EnableScheduling
 public class MainBackend {
 
     public static void main(String[] args) {
-        // Iniciar Spring Boot como una aplicación web (servidor)
-        // Si no se especifica, por defecto es SERVLET si spring-web está presente
         SpringApplication.run(MainBackend.class, args);
         System.out.println("Backend Spring Boot iniciado.");
-        // Puedes añadir un log o mensaje indicando que el backend está listo
     }
 }
